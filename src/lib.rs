4cf5f72fe5da24fb5f6a9c2d15e587eceb588e98
Björn Guth <scatty@bguth.de>
2020-08-22 21:24:33 +0200

    Enable reading of the access token from pass

---
+++ src/lib.rs
@@ -0,0 +1,104 @@
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::fmt;
+use std::fs::File;
+use std::io::prelude::*;
+use std::process::Command;
+
+#[derive(Debug)]
+pub enum MatrixAPIError {
+    ServerNotDefined(String),
+    ConfigFileError(String),
+    AccessTokenError(String),
+}
+
+impl std::error::Error for MatrixAPIError {}
+
+impl fmt::Display for MatrixAPIError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            MatrixAPIError::ServerNotDefined(s) => {
+                write!(f, "The given server {} is not defined", s)
+            }
+            MatrixAPIError::ConfigFileError(s) => {
+                write!(f, "There was a error with the config file: {}", s)
+            }
+            MatrixAPIError::AccessTokenError(s) => {
+                write!(f, "There was an error reading the access token: {}, s", s)
+            }
+        }
+    }
+}
+
+impl From<std::io::Error> for MatrixAPIError {
+    fn from(error: std::io::Error) -> Self {
+        MatrixAPIError::ConfigFileError(error.to_string())
+    }
+}
+
+impl From<xdg::BaseDirectoriesError> for MatrixAPIError {
+    fn from(error: xdg::BaseDirectoriesError) -> Self {
+        MatrixAPIError::ConfigFileError(error.to_string())
+    }
+}
+
+impl From<toml::de::Error> for MatrixAPIError {
+    fn from(error: toml::de::Error) -> Self {
+        MatrixAPIError::ConfigFileError(error.to_string())
+    }
+}
+
+impl From<std::str::Utf8Error> for MatrixAPIError {
+    fn from(error: std::str::Utf8Error) -> Self {
+        MatrixAPIError::AccessTokenError(error.to_string())
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    default_server: String,
+    server: HashMap<String, ServerConfig>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+struct ServerConfig {
+    server_name: String,
+    server_url: String,
+    pass_access_token: String,
+}
+
+fn get_server_config(server: Option<&str>) -> Result<ServerConfig, MatrixAPIError> {
+    let xdg_dirs = xdg::BaseDirectories::with_prefix("matrixapirs")?;
+    let config_path = xdg_dirs
+        .find_config_file("config.toml")
+        .ok_or_else(|| MatrixAPIError::ConfigFileError("config file does not exist".to_string()))?;
+    let mut config_file = File::open(config_path)?;
+    let mut config_string = String::new();
+    config_file.read_to_string(&mut config_string)?;
+    let config: Config = toml::from_str(&config_string)?;
+    match server {
+        Some(s) => match config.server.get(&s.to_string()) {
+            Some(c) => Ok(c.clone()),
+            None => Err(MatrixAPIError::ServerNotDefined(s.to_string())),
+        },
+        None => match config.server.get(&config.default_server) {
+            Some(c) => Ok(c.clone()),
+            None => Err(MatrixAPIError::ServerNotDefined(config.default_server)),
+        },
+    }
+}
+
+fn get_access_token(server_config: &ServerConfig) -> Result<String, MatrixAPIError> {
+    let pass_cmd = Command::new("pass")
+        .arg(server_config.pass_access_token.clone())
+        .output()?;
+    Ok(std::str::from_utf8(&pass_cmd.stdout)?.trim().into())
+}
+
+pub fn get_server_version(server_name: Option<&str>) -> Result<(), MatrixAPIError> {
+    let server_config = get_server_config(server_name)?;
+    let access_token = get_access_token(&server_config)?;
+    println!("{:?}", access_token);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let server_arg = Arg::with_name("server")
+        .short("s")
+        .long("server")
+        .value_name("SERVER")
+        .help("The server to be used")
+        .takes_value(true);
+
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .subcommand(
+            SubCommand::with_name("version")
+                .author(crate_authors!())
+                .version(crate_version!())
+                .about("Get the current version of the synapse server")
+                .arg(&server_arg),
+        )
+        .get_matches();
+
+    println!("{:?}", matches);
+
+    if let Some(matches) = matches.subcommand_matches("version") {
+        match matrixapi::get_server_version(matches.value_of("server")) {
+            Ok(_) => (),
+            Err(e) => eprintln!("Something went wrong: {}", e),
+        };
+    }
 }
